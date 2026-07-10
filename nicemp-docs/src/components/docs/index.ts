53bfe7bf13e895b59f5e27501988e7716bf93c52
Nice Emp <301795395+Nicempdotcom@users.noreply.github.com>
2026-07-10 16:28:25 -0300

    Delete nicemp-docs directory

--- nicemp-docs/src/components/docs/index.ts
+++
@@ -1,5 +0,0 @@
-export { DocCard, FeatureCard, LinkCard } from './Card';
-export { default as Section } from './Section';
-export { default as InfoBox } from './InfoBox';
-export { default as StatusBadge } from './StatusBadge';
-export { default as RiskBadge } from './RiskBadge';
